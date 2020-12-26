42ec3247d635c078bad5f79f18f1a1e7d3fd4651
Asa Salo <anttimeister@gmail.com>
2020-12-26 03:04:18 +0200

    Handling exceptions, formatting

--- kusibot.py
+++ kusibot.py
@@ -6,35 +6,46 @@ import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv("DISCORD_TOKEN")
 # GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-  print(f'{client.user.name} has connected to Discord!')
+    print(f"{client.user.name} has connected to Discord!")
+
 
 @client.event
 async def on_member_join(member):
-  await member.create_dm()
-  await member.dm_channel.send(
-    f'Hi {member.name}, welcome to {client.user.name} playground!'
-  )
+    await member.create_dm()
+    await member.dm_channel.send(
+        f"Hi {member.name}, welcome to {client.user.name} playground!"
+    )
+
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
+    if message.author == client.user:
+        return
 
-  tussi = [
-    'Tussi.',
-    'Yeah boii!',
-    'I am the great %s' % client.user.name
-  ]
+    tussi = ["Tussi.", "Yeah boii!", "I am the great %s" % client.user.name]
 
-  if message.content == 'kusibot':
-    response = random.choice(tussi)
-    await message.channel.send(response)
+    if message.content == "kusibot":
+        response = random.choice(tussi)
+        await message.channel.send(response)
+    elif message.content == "raise-exception":
+        raise discord.DiscordException
+
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open("kusibot-error.log", "a") as f:
+        if event == "on_message":
+            f.write(f"Unhandled message: {args[0]}\n")
+        else:
+            raise
+
 
 client.run(TOKEN)
